6d4cd18f3ba880974601d90a5a7d4925422d51db
Elena Mogilevich <nervuse@gmail.com>
2022-05-17 14:57:42 +0200

    Initial Commit

---
+++ ContactsAppSwiftUI/ContactsAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsAppSwiftUIApp.swift
+//  ContactsAppSwiftUI
+//
+//  Created by elena on 17.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactsAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
